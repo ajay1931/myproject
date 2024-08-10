38fcb40b776c46774ccedeb911d2b4311653c10a
ajay1931 <ajayprakash032@gmail.com>
2024-08-10 13:40:07 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import Home from './Home';
+import Home1 from './HomeOne';
 import Parrent from './Parrent';
-  function App() {
-    return (
-      <div id='home'>
-        <h1>Basics</h1>
-        <Home/>
-        
-        <Parrent/>
-      </div>
+import UseEffect from './UseEffect';
+function App() {
+  return (
+    <div id='home'>
+      <h1 className='heading'>Basics</h1>
+      <Home />
+      
+      <Parrent />
+      <h1 className='heading'>useState</h1>
+      <Home1 />
+      <h1 className='heading'>useEffect</h1>
+      <UseEffect />
+    </div>
 
 
-    );
-  }
+  );
+}
 
 export default App;
