b57857c36bcae4a9b5d96ac89c37513b0759564d
ajay1931 <ajayprakash032@gmail.com>
2024-08-12 11:52:27 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Home from './Home';
 import Home1 from './HomeOne';
 import Parrent from './Parrent';
 import UseEffect from './UseEffect';
+import Memo from './Memo';
+
 function App() {
   return (
     <div id='home'>
       <h1 className='heading'>Basics</h1>
       <Home />
-      
+
       <Parrent />
       <h1 className='heading'>useState</h1>
       <Home1 />
       <h1 className='heading'>useEffect</h1>
       <UseEffect />
+      <h1 className='heading'>useMemo</h1>
+      <Memo />
     </div>
 
 
