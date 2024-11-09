087c81031aaeae63466ca9f415e9cb3cd73e9789
ajay1931 <ajayprakash032@gmail.com>
2024-11-09 12:55:18 +0530

    update

---
+++ src/Api_call.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Api_call = () => {
+  return (
+    async function api() {
+        const 
+        
+    }
+    <div>
+      
+    </div>
+  )
+}
+
+export default Api_call
